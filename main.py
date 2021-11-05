0f660466ae39af927f015d66e0f1c77a4e3f6eab
sachin.mi20 <sachin.mi20@iiitmk.ac.in>
2021-11-05 23:30:33 +0530

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import PIL
+
+from PIL import Image
 #open image
-img= PIL.Image.opoen('c1.png')
+img= Image.open('c1.png')
 
-def pixelate():
+def pixelate(pix_list):
+    initial_x=0
+    initial_y=0
+    for i in pix_list:
+        for j in range(initial_x,pix_list[0]):
+            for k in range(initial_y,pix_list[1]):
+                pass
     pass
-def grid(img):
+def grid(img,m,n):
     width,height=img.size
-    
+    m_gradient=round(width/m)
+    n_gradient=round(height/n)
+    pix_list=[]
+    for i in range(m_gradient,width+m_gradient,m_gradient):
+        for j in range(n_gradient,height+n_gradient,n_gradient):
+            pix_list.append([i,j])
+    return pix_list
 
-grid(img)
+
+print(grid(img,100,100))
